9be8a50d53fa401ff51d9f1f8605124a0c2b5f16
Beardeadbear <beardeadbeardead@gmail.com>
2023-08-01 09:44:11 +0300

    13. Арифметические операторы

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // 12. Переменные и значения
-let a = 1;
-console.log(a);
-a = 'Test';
-console.log(a);
+// let a = 1;
+// console.log(a);
+// a = 'Test';
+// console.log(a);
+
+//  13. Арифметические операторы
+const  width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth2 / newWidth;
+const volume = 2 * 2 * 2;
+const volume2 = 2 ** 3;
+
+console.log(volume2);
+
+const firstName = 'Stepan';
+const lastName = 'Bandera';
+console.log(firstName + ' ' + lastName + ', ' + 9000);
