ec9e7e01b64a5e696ff57e610e560870f7af1310
Beardeadbear <beardeadbeardead@gmail.com>
2023-08-01 10:10:58 +0300

    14. Операторы присваивания и сравнения

--- app.js
+++ app.js
@@ -5,17 +5,34 @@
 // console.log(a);
 
 //  13. Арифметические операторы
-const  width = 10;
-const height = 5;
-const space = width * height;
-const newWidth = width - 4;
-const newWidth2 = width + 4;
-const division = newWidth2 / newWidth;
-const volume = 2 * 2 * 2;
-const volume2 = 2 ** 3;
+// const  width = 10;
+// const height = 5;
+// const space = width * height;
+// const newWidth = width - 4;
+// const newWidth2 = width + 4;
+// const division = newWidth2 / newWidth;
+// const volume = 2 * 2 * 2;
+// const volume2 = 2 ** 3;
 
-console.log(volume2);
+// console.log(volume2);
 
-const firstName = 'Stepan';
-const lastName = 'Bandera';
-console.log(firstName + ' ' + lastName + ', ' + 9000);
+// const firstName = 'Stepan';
+// const lastName = 'Bandera';
+// console.log(firstName + ' ' + lastName + ', ' + 9000);
+
+//14. Операторы присваивания и сравнения
+// Присваивание
+let age = 18 + 2;
+age += 5; // age = age + 5
+age *= 2; // age = age * 2
+age /= 2;
+age ++ // age = age + 1
+age -- //age = age - 1
+console.log('I am ' + age + ' years old');
+// Сравнение:
+let Stepan = 25;
+console.log(Stepan < age);
+console.log(Stepan <= age);
+console.log(Stepan > age);
+console.log(Stepan >= age);
+console.log(Stepan == age);
