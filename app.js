bfc4b3e3bfeed7c06f36599594b712b73b97d775
Beardeadbear <beardeadbeardead@gmail.com>
2023-08-18 10:36:27 +0300

    58. Callback

--- app.js
+++ app.js
@@ -985,3 +985,37 @@ function display(message) {
 // }
 
 // console.log(avarageOperations(operations))
+
+// Функции высшего порядка
+
+//58. Callback
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function power(a, b) {
+//     return a ** b;
+// }
+
+// // высший порядко
+
+// function calculate(a, b, fn) {
+
+//     console.log(fn.name);
+//     const res = fn(a, b)
+//     return res;
+// } 
+
+// let  res = calculate(3, 5, add);
+// console.log(res);
+
+// res = calculate(3, 5, subtract);
+// console.log(res);
+
+// res = calculate(3, 5, power);
+// console.log(res);
